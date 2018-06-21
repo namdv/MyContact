f70538f3fd69e6776e24b4ec68fea1556465c15e
namdv <namdovana2tt@gmail.com>
2018-06-21 11:10:53 +0700

    init comment

--- src/main/java/com/example/mycontact/model/Contact.java
+++ src/main/java/com/example/mycontact/model/Contact.java
@@ -18,14 +18,8 @@ public class Contact {
     @Email
     @Column(name = "email", nullable = false)
     private String email;
-    @Column(name = "password")
-    private String password;
     @Column(name = "phone")
     private String phone;
-    @ManyToMany
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
 
     public int getId() {
         return id;
@@ -51,14 +45,6 @@ public class Contact {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getPhone() {
         return phone;
     }
@@ -67,11 +53,5 @@ public class Contact {
         this.phone = phone;
     }
 
-    public Set<Role> getRoles() {
-        return roles;
-    }
 
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
